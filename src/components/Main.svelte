0aea2eb3fbd2ca30095b630fe62bd54b6a880453
Md. Ziaur Rahman <ziaur@gmail.com>
2024-04-01 12:34:08 +0600

    Blog

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,5 +1,7 @@
 <script>
     import Step from "./Step.svelte";
+    import { Blockquote } from 'flowbite-svelte';
+    import { QuoteSolid } from 'flowbite-svelte-icons';
 
     let steps = [
         {
@@ -91,6 +93,29 @@
             />
         </div> -->
     </section>
+
+    <section>
+          
+          <figure class="mx-auto max-w-screen-md text-center">
+            <QuoteSolid class="mx-auto mb-3 w-12 h-12 text-gray-400 dark:text-gray-600" />
+            <Blockquote alignment="center" size="2xl">"Flowbite is just awesome. It contains tons of predesigned components and pages starting from login screen to complex dashboard. Perfect choice for your next SaaS application."</Blockquote>
+            <figcaption class="flex justify-center items-center mt-6 space-x-3 rtl:space-x-reverse">
+              <img class="w-6 h-6 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="Micheal Gough profile" />
+              <div class="flex items-center divide-x-2 rtl:divide-x-reverse divide-gray-500 dark:divide-gray-700">
+                <cite class="pe-3 font-medium text-gray-900 dark:text-white">Micheal Gough</cite>
+                <cite class="ps-3 text-sm font-light text-gray-500 dark:text-gray-400">CEO at Google</cite>
+              </div>
+            </figcaption>
+          </figure>
+
+
+
+
+    </section>
+
+
+
+
     <section class="py-20 lg:py-32 flex flex-col gap-24" id="projects">
         <div class="flex flex-col gap-2 text-center">
             <h6 class="text-large sm:text-xl md:text-2xl">
